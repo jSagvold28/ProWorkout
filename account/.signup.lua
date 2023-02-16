64cad539a571e2b2654c67b055123f53e961f3fe
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-02-16 16:13:06 -0600

    added 2fa

--- account/.signup.lua
+++ account/.signup.lua
@@ -1,38 +1,42 @@
-io.write("Email: ")
+print("Email:")
 local email = io.read()
-
-io.write("Age: ")
-local userAge = tonumber(io.read())
-
-if userAge and userAge <= 12 then
-    print("You are not able to use a ProWorkout account. You must be 13 or older.")
-end
-
-io.write("Password: ")
+ 
+print("Password:")
 local password = io.read()
 
-print("Would you like to create an account?")
-print("Type 'yes' to create an account. Type 'no' to not create an account.")
-local input = io.read()
+local twoFactorCode = true
+local TwoFactorFile = "2FactorCode.txt"
 
-if input == "yes" then
+if twoFactorCode then
+    if io.open(TwoFactorFile, "r") then
+        local file = io.open(TwoFactorFile, "r")
+        local storedCode = file:read("*all")
+        file:close()
 
-    local function createUserAccountAndFile()
+        print("Enter your saved 2FA code:")
+        local inputed2FACode = io.read()
 
-        local userDataFile = io.open("AccountData.txt", "w")
-    
-        userDataFile:write("Email: " .. email .. "\n")
-        userDataFile:write("Age: " .. userAge .. "\n")
-        userDataFile:write("Password: " .. password .. "\n")
-    
-        userDataFile:close()
+        if inputed2FACode == storedCode then
+            print("2FA code has been confirmed!")
+        end
     end
-    
-    createUserAccountAndFile()
+end
 
-elseif input == "no" then
-    print("Account was not created, due to: your input")
+local userAccountFile = "AccountData.txt"
 
-elseif input == "" then
-    print("Account not created due to an: error: user or service.")
+if io.open(userAccountFile, "r") then
+    local file = io.open(userAccountFile, "r")
+    local data = file:read("*all")
+    file:close()
+
+    local emailLine = string.match(data, "Email:([^\r\n]+)")
+    local passwordLine = string.match(data, "Password:([^\r\n]+)")
+
+    if emailLine == email and passwordLine == password then
+        print("Login successful!")
+    else
+        print("Login successful!")
+    end
+else
+    print("User account file not found.")
 end
