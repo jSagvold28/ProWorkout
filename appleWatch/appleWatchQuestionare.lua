546766668f544e9524629a764061c55d6403c388
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-02-17 07:24:50 -0600

    initial commit

---
+++ appleWatch/appleWatchQuestionare.lua
@@ -0,0 +1,27 @@
+local function appleWatchQuestionnaire()
+    local appleWatchDataFile = io.open("AppleWatchData.txt", "w")
+
+    io.write("What Apple Watch do you use: ")
+    local appleWatchModel = io.read()
+
+    io.write("Your fitness goals:\n")
+
+    io.write("Calories burnt per day: ")
+    local caloriesBurntPerDayAppleWatch = io.read()
+
+    io.write("Exercise minutes per day: ")
+    local exerciseMinutesPerDayAppleWatch = io.read()
+
+    io.write("Stand hours per day: ")
+    local standHoursAppleWatch = io.read()
+
+    -- Write data to file --
+    appleWatchDataFile:write("Apple Watch model: " .. appleWatchModel .. "\n")
+    appleWatchDataFile:write("Calories burnt: " .. caloriesBurntPerDayAppleWatch .. " (per day)" .."\n")
+    appleWatchDataFile:write("Exercise minutes: " .. exerciseMinutesPerDayAppleWatch .. " (per day)" .."\n")
+    appleWatchDataFile:write("Stand hours: " .. standHoursAppleWatch .. " (per day)" .."\n")
+
+    appleWatchDataFile:close()
+end
+
+appleWatchQuestionnaire()
